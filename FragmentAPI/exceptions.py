eafdf08ba4e8a09d7544f90b8c70c7a0a10625fb
S1qwy <amirhansuper75@gmail.com>
2026-01-25 21:52:37 +0300

    Update exceptions.py v3.2.0

--- FragmentAPI/exceptions.py
+++ FragmentAPI/exceptions.py
@@ -71,3 +71,36 @@ class WalletError(FragmentAPIException):
     Raised when wallet operations fail
     """
     pass
+
+
+class InvalidWalletVersionError(WalletError):
+    """
+    Raised when invalid wallet version is specified
+    
+    Attributes:
+        version: The invalid version that was provided
+        supported_versions: List of supported wallet versions
+    """
+    
+    SUPPORTED_VERSIONS = {
+        'V4R2': 'WalletV4R2 - Most common wallet version',
+        'V5R1': 'WalletV5R1 - Latest wallet version (also known as W5)',
+        'W5': 'WalletV5R1 - Alias for V5R1',
+        'V3R2': 'WalletV3R2 - Legacy wallet version',
+        'V3R1': 'WalletV3R1 - Legacy wallet version'
+    }
+    
+    def __init__(self, version: str):
+        self.version = version
+        self.supported_versions = self.SUPPORTED_VERSIONS
+        
+        versions_info = "\n".join([
+            f"  - {name}: {desc}" 
+            for name, desc in self.SUPPORTED_VERSIONS.items()
+        ])
+        
+        message = (
+            f"Invalid wallet version: '{version}'\n"
+            f"Supported wallet versions:\n{versions_info}"
+        )
+        super().__init__(message)
