a3127601f2043287c28d387c71f08332cc5f6fe1
root <root@tender-mccarthy.1cent.network>
2026-08-17 20:17:45 +0000

    refactor: update methods __init__.py to import all purchase utilities from purchase module
    - Import batch_purchase directly from methods.purchase
    - Remove batch_purchase submodule reference

--- FragmentAPI/methods/__init__.py
+++ FragmentAPI/methods/__init__.py
@@ -4,8 +4,14 @@ Method exports for Fragment API library.
 Purchase methods, giveaways, marketplace operations, and search.
 """
 
-from FragmentAPI.methods.purchase import purchase, purchase_stars, purchase_premium, topup_gram, topup_ton
-from FragmentAPI.methods.batch_purchase import batch_purchase
+from FragmentAPI.methods.purchase import (
+    batch_purchase,
+    purchase,
+    purchase_premium,
+    purchase_stars,
+    topup_gram,
+    topup_ton,
+)
 from FragmentAPI.methods.giveaway import giveaway_premium, giveaway_stars
 from FragmentAPI.methods.place_bid import place_bid
 from FragmentAPI.methods.search import search_gifts, search_numbers, search_usernames
