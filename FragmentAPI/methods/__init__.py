223529e5b08a9cbece3753363a0fd6684cc7240d
S1qwy <s1qwy@internet.ru>
2026-08-19 18:49:14 +0000

    export new marketplace methods

--- FragmentAPI/methods/__init__.py
+++ FragmentAPI/methods/__init__.py
@@ -15,18 +15,36 @@ from FragmentAPI.methods.purchase import (
 from FragmentAPI.methods.giveaway import giveaway_premium, giveaway_stars
 from FragmentAPI.methods.place_bid import place_bid
 from FragmentAPI.methods.search import search_gifts, search_numbers, search_usernames
+from FragmentAPI.methods.marketplace import (
+    cancel_auction,
+    confirm_ads_withdrawal,
+    get_gateway_price,
+    init_ads_withdrawal,
+    make_offer,
+    recharge_gateway,
+    subscribe_to_item,
+    unsubscribe_from_item,
+)
 
 __all__ = [
     "batch_purchase",
+    "cancel_auction",
+    "confirm_ads_withdrawal",
+    "get_gateway_price",
     "giveaway_premium",
     "giveaway_stars",
+    "init_ads_withdrawal",
+    "make_offer",
     "place_bid",
     "purchase",
     "purchase_premium",
     "purchase_stars",
+    "recharge_gateway",
     "search_gifts",
     "search_numbers",
     "search_usernames",
+    "subscribe_to_item",
     "topup_gram",
     "topup_ton",
+    "unsubscribe_from_item",
 ]
